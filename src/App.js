5e5becd10344bf31c8ebc63e6696894535e95d13
Naitik Verma <naitikv2311@gmail.com>
2025-04-05 20:00:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { getDatabase, ref, push } from 'firebase/database';
+import { app } from './firebase';
 import './App.css';
 
+const db = getDatabase(app);
+
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    scholarNo: '',
+    email: '',
+    erdid: '',
+    Branch:''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   const userinfo=ref(db,'users');
+   push(userinfo,formData);
+    alert("Data submitted successfully!");
+    setFormData({ name: '', scholarNo: '', email: '', erdNo: '',branch:'' }); // reset
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <h2>Student Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
+        <input type="text" name="scholarNo" placeholder="Scholar Number" value={formData.scholarNo} onChange={handleChange} required />
+        <input type="email" name="email" placeholder="Email ID" value={formData.email} onChange={handleChange} required />
+        <input type="text" name="erdNo" placeholder="ERD ID" value={formData.erdNo} onChange={handleChange} required />
+        <input type="text" name="branch" placeholder="Branch" value={formData.branch} onChange={handleChange} required />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyALQ4wQRQ5jp9cwLWmJiqYbi4tSQtv0xK0",
+    authDomain: "naitik-5bb0b.firebaseapp.com",
+    databaseURL: "https://naitik-5bb0b-default-rtdb.firebaseio.com",
+    projectId: "naitik-5bb0b",
+    storageBucket: "naitik-5bb0b.firebasestorage.app",
+    messagingSenderId: "755786221452",
+    appId: "1:755786221452:web:fc7601642c4127d757f8a9",
+    databaseURL:"https://naitik-5bb0b-default-rtdb.firebaseio.com"
+  };
+
+  export const app=initializeApp(firebaseConfig);
